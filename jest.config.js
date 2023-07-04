5c9e2611c5b13a4bfd580d5b610782872c260f40
Khaled AbdRahem <aa.khaled11@gmail.com>
2023-07-04 16:26:32 +0300

    add: add webpack basic setup

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  modulePaths: ['/shared/vendor/modules'],
+  moduleFileExtensions: ['js', 'jsx'],
+  moduleDirectories: ['node_modules', 'bower_components', 'shared'],
+
+  moduleNameMapper: {
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  transform: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/fileTransformer.js',
+
+    '^react(.*)$': '<rootDir>/vendor/react-master$1',
+    '^config$': '<rootDir>/configs/app-config.js',
+  },
+};
